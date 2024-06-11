b488cd6880afb9f25cac8eba58d4c38b86485bac
unknown <Muhammad Hassan Raza>
2024-06-11 08:45:21 +0500

    Hassan Raza Store

---
+++ app.js
@@ -0,0 +1,37 @@
+var loginbtn = document.getElementById('loginbtn')
+var accountbtn = document.getElementById('accountbtn')
+
+function donebtn(){
+    var username = document.getElementById('username')
+    var email = document.getElementById('email')
+    var password = document.getElementById('password')
+    localStorage.setItem('name',username.value)
+    localStorage.setItem('email',email.value)
+    localStorage.setItem('password',password.value)
+    alert('Account Created ✅')
+    username.value = ''
+    email.value = ''
+    password.value = ''
+    window.location.href = 'index.html'
+}
+
+loginbtn.addEventListener('click',()=>{
+    var getemail = localStorage.getItem('email')
+    var getpass = localStorage.getItem('password')
+
+    var loginE = document.getElementById('loginE').value
+    var loginP = document.getElementById('loginP').value
+
+    if(loginE == getemail && loginP == getpass){
+        window.open('dashboard.html')
+    }
+    else{
+        alert('Wrong Email or Password ❗')
+        loginE = ''
+        loginP = ''
+    }
+})
+
+accountbtn.addEventListener('click',()=>{
+    window.location.href = 'account.html'
+})
